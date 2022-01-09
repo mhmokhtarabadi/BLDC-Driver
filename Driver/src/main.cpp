e6b620621a5f8d88c7eb64f716744b487c1412a2
mohamadhasan-m <mh.mokhtarabadi@gmail.com>
2022-01-09 13:09:44 +0330

    delay added between states

--- Driver/src/main.cpp
+++ Driver/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 
-const int APWM_PIN = 3;
-const int BPWM_PIN = 5;
-const int CPWM_PIN = 6;
+const uint8_t APWM_PIN = 3;
+const uint8_t BPWM_PIN = 5;
+const uint8_t CPWM_PIN = 6;
 
-const int Aen = DDB0;         // D8
-const int Ben = DDB1;         // D9
-const int Cen = DDB2;         // D10
+const uint8_t Aen = DDB0;         // D8
+const uint8_t Ben = DDB1;         // D9
+const uint8_t Cen = DDB2;         // D10
 
-const int s = 50;
+const uint8_t s = 50;
+const uint8_t d = 10;
 
 void setup() {
   DDRB = _BV(Aen) | _BV(Ben) | _BV(Cen);
@@ -25,34 +26,40 @@ void loop() {
   PORTB &= ~_BV(Cen);
   analogWrite(APWM_PIN, s);
   analogWrite(BPWM_PIN, 0);
+  delay(d);
 
   // state 2
   PORTB |= _BV(Aen) | _BV(Cen);
   PORTB &= ~_BV(Ben);
   analogWrite(APWM_PIN, s);
   analogWrite(CPWM_PIN, 0);
+  delay(d);
 
   // state 3
   PORTB |= _BV(Ben) | _BV(Cen);
   PORTB &= ~_BV(Aen);
   analogWrite(BPWM_PIN, s);
   analogWrite(CPWM_PIN, 0);
+  delay(d);
 
   // state 4
   PORTB |= _BV(Ben) | _BV(Aen);
   PORTB &= ~_BV(Cen);
   analogWrite(BPWM_PIN, s);
   analogWrite(APWM_PIN, 0);
+  delay(d);
 
   // state 5
   PORTB |= _BV(Cen) | _BV(Aen);
   PORTB &= ~_BV(Ben);
   analogWrite(CPWM_PIN, s);
   analogWrite(APWM_PIN, 0);
+  delay(d);
 
   // state 6
   PORTB |= _BV(Cen) | _BV(Ben);
   PORTB &= ~_BV(Aen);
   analogWrite(CPWM_PIN, s);
   analogWrite(BPWM_PIN, 0);
+  delay(d);
 }
